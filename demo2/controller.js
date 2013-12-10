7cb6d1a64e2a2a4e0092f8dc067b22e88bd6adab
lee <ljm@it-innovation.soton.ac.uk>
2013-12-10 16:45:15 +0000

    creating a directive for the rendering

--- demo2/controller.js
+++ demo2/controller.js
@@ -1,6 +1,17 @@
+var app = angular.module('threedee', []);
 
-function MainController($scope) {
+app.controller('MainController', function($scope) {
     $scope.bounds = [20, 30, 60, 60];
     $scope.regions = [[[42,42], [45,42], [45,46], [42,46]],
         [[53,53], [55,53], [57,55], [55,57], [53,57]]];
-}
+});
+
+app.directive('buildingview', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            bounds: '='
+        },
+        template: '<ul><div ng-repeat="bi in bounds"><li>{{bi}}</li></div></ul>'
+    }
+});
